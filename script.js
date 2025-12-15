65269ebfff4830fbdbead402b6991dbfc4f3caef
LalyyCortelassi <laryssa.cortelassi.dev@gmail.com>
2025-12-15 11:14:54 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,29 +1,24 @@
-// Lista com 100 códigos secretos pré-definidos
 const codigosSecretos = [
   "A1B2C",
   "K1L2M",
   "U1V2W",
   "E1F2G",
   "O1P2Q",
-  // Adicione mais códigos conforme necessário
 ];
-
-const codigoSecreto = codigosSecretos[Math.floor(Math.random() * codigosSecretos.length)];
-
-// Exibe o código secreto na tela
-document.getElementById("codigoExibido").textContent = codigoSecreto;
 
 function verificarCodigo() {
   const codigoInserido = document.getElementById("codigoInput").value;
   const messageDiv = document.getElementById("message");
 
   if (codigosSecretos.includes(codigoInserido)) {
-    messageDiv.innerHTML = "<div class='success-message'>Código correto! Você pode avançar para a próxima página.</div>";
-    // Redireciona para a próxima página (por enquanto está vazia)
+    
+    messageDiv.innerHTML = "<div id='success-message'>Código correto! Você será redirecionado.</div>";
+    
     setTimeout(() => {
-      window.location.href = "pagina2.html"; // Altere o caminho conforme necessário
+      window.location.href = "pagina2.html"; 
     }, 2000);
   } else {
+    
     const zoeiraMessages = [
       "Sabe nem digitar? O código que você digitou está incorreto!",
       "Acho que você está perdendo seu tempo! Código errado!",
@@ -31,7 +26,8 @@ function verificarCodigo() {
       "Eita! Aí não, hein! Código incorreto!",
       "Nem acertou um! Sério mesmo?"
     ];
+
     const randomZoeira = zoeiraMessages[Math.floor(Math.random() * zoeiraMessages.length)];
-    messageDiv.innerHTML = "<div class='error-message'>" + randomZoeira + "</div>";
+    messageDiv.innerHTML = "<div id='error-message'>" + randomZoeira + "</div>";
   }
 }
